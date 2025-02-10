82863cf65ba13a192f9134e787979a3cf4b33649
saylees9312 <saylees9312@naver.com>
2025-02-10 19:11:06 +0900

    css완성

--- js/script.js
+++ js/script.js
@@ -1,8 +1,47 @@
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>;
-
-const swiper = new Swiper(".swiper", {
+const visualSlider = new Swiper(".visual-wrap", {
     // Optional parameters
     loop: true,
+    autoplay: true,
+    speed: 3000,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickerble: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".visual-btn-prev",
+        prevEl: ".visual-btn-next",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
+
+const swiper2 = new Swiper(".business-wrap", {
+    loop: true,
+    autoplay: true,
+    speed: 1200,
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
+
+const swiper3 = new Swiper(".news-wrap", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
 
     // If we need pagination
     pagination: {
